f52ff2315dfdcc586e6d1719a280f7442fbc8fe6
RaghuRam2005 <atommail20@gmail.com>
2025-10-11 14:00:49 +0530

    added new methods to __init__

--- obj_yolo/__init__.py
+++ obj_yolo/__init__.py
@@ -1,12 +1,15 @@
 # __init__.py
-from .server import Server
-from .strategy import Strategy
-from .client import Client
-from .dataset import load_data
+from .server import Server, FedWegServer, FedTagServer
+from .strategy import Strategy, FedTag, FedWeg
+from .dataset import KittiData, BddData
 
 __all__ = [
     "Server",
+    "FedWegServer",
+    "FedTagServer",
     "Strategy",
-    "Client",
-    "load_data",
+    "FedTag",
+    "FedWeg",
+    "KittiData",
+    "BddData"
 ]
